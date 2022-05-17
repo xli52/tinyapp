f8d4a7cd09ce152c761049f82c97b44ff9576247
Xiang Li <lxmichael52@gmail.com>
2022-05-17 01:44:14 -0700

    implemented generate random string

--- express_server.js
+++ express_server.js
@@ -1,30 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
-
-app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -42,3 +45,12 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+function generateRandomString() {
+  const charString = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const randomString = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * charString.length);
+    randomString += charString.charAt(randomIndex);
+  }
+  return randomString;
+}
